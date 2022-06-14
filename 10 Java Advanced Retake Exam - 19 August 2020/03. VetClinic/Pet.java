f3c86e0cc3f2ce5db95b9e6585ad921cd08eb563
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-14 12:34:40 +0300

    Create Pet.java

---
+++ 10 Java Advanced Retake Exam - 19 August 2020/03. VetClinic/Pet.java
@@ -0,0 +1,43 @@
+package vetClinic;
+
+public class Pet {
+    private String name;
+    private int age;
+    private String owner;
+
+    public Pet(String name, int age, String owner) {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        //"{name} {age} ({owner})"
+        return name + " " + age + " (" + owner + ")";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
