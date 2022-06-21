00849e1a3de738cf5249ba2c4f326d96a9a1733e
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-21 08:48:57 +0300

    Create Present.java

---
+++ 13 Java Advanced Retake Exam - 17 Dec 2019/03 Christmas/Present.java
@@ -0,0 +1,31 @@
+package christmas;
+
+public class Present {
+    private String name;
+    private double weight;
+    private String gender;
+
+    public Present(String name, double weight, String gender) {
+        this.name = name;
+        this.weight = weight;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        //"Present {name} ({weight}) for a {gender}"
+        return String.format("Present %s (%.2f) for a %s", name, weight, gender);
+    }
+}
