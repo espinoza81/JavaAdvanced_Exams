fd06e41434ddf1966cb9ed3aeabfb699d92c87da
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-24 10:28:19 +0300

    Update 01FoodFinder.java

--- 02 Java Advanced Exam - 19 February 2022/01FoodFinder.java
+++ 02 Java Advanced Exam - 19 February 2022/01FoodFinder.java
@@ -1,42 +1,46 @@
-package Exams;
+package Exams_maps;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FoodFinder {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        ArrayDeque<String> vowels = new ArrayDeque<>(); //queue
+
+        ArrayDeque<String> vowels = Arrays.stream(console.nextLine().split("\\s+")).collect(Collectors.toCollection(ArrayDeque::new)); //queue
+
         ArrayDeque<String> consonants = new ArrayDeque<>(); //stack
-        String[] vowel = console.nextLine().split("\\s+");
-        String[] consonant = console.nextLine().split("\\s+");
-        Collections.addAll(vowels, vowel);
-        Arrays.stream(consonant).forEach(consonants::push);
-        Set<String> pear = new HashSet<>();
-        Set<String> flour = new HashSet<>();
-        Set<String> pork = new HashSet<>();
-        Set<String> olive = new HashSet<>();
+        Arrays.stream(console.nextLine().split("\\s+")).forEach(consonants::push);
+
+        Map<String, Set<String>> word = new LinkedHashMap<>();
+        word.put("pear", new HashSet<>());
+        word.put("flour", new HashSet<>());
+        word.put("pork", new HashSet<>());
+        word.put("olive", new HashSet<>());
+
         List<String> foundWord = new ArrayList<>();
 
         while (!consonants.isEmpty()) {
             String singleVowel = vowels.poll();
             String singleConsonant = consonants.pop();
-            if("pear".contains(Objects.requireNonNull(singleVowel))) pear.add(singleVowel);
-            if("pear".contains(Objects.requireNonNull(singleConsonant))) pear.add(singleConsonant);
-            if("flour".contains(Objects.requireNonNull(singleVowel))) flour.add(singleVowel);
-            if("flour".contains(Objects.requireNonNull(singleConsonant))) flour.add(singleConsonant);
-            if("pork".contains(Objects.requireNonNull(singleVowel))) pork.add(singleVowel);
-            if("pork".contains(Objects.requireNonNull(singleConsonant))) pork.add(singleConsonant);
-            if("olive".contains(Objects.requireNonNull(singleVowel))) olive.add(singleVowel);
-            if("olive".contains(Objects.requireNonNull(singleConsonant))) olive.add(singleConsonant);
+            for (var entry : word.entrySet()) {
+                if(entry.getKey().contains(singleVowel)) {
+                    entry.getValue().add(singleVowel);
+                }
+                if(entry.getKey().contains(singleConsonant)) {
+                    entry.getValue().add(singleConsonant);
+                }
+            }
             vowels.offer(singleVowel);
         }
-        if(pear.size()==4) foundWord.add("pear");
-        if(flour.size()==5) foundWord.add("flour");
-        if(pork.size()==4) foundWord.add("pork");
-        if(olive.size()==5) foundWord.add("olive");
+
+        for (var entry : word.entrySet()) {
+            if(entry.getKey().length() == entry.getValue().size()) {
+                foundWord.add(entry.getKey());
+            }
+        }
 
         System.out.println("Words found: " + foundWord.size());
         System.out.println(String.join(System.lineSeparator(), foundWord));
-
     }
 }
